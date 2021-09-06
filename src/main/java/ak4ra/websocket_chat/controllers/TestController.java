7851c0255003836d5515a0e56cf194b6b35d637a
ak4ra <aryjna1@gmail.com>
2021-09-06 11:56:11 +0300

    Add stomp_test client subscribe

--- src/main/java/ak4ra/websocket_chat/controllers/TestController.java
+++ src/main/java/ak4ra/websocket_chat/controllers/TestController.java
@@ -3,18 +3,28 @@ package ak4ra.websocket_chat.controllers;
 import ak4ra.websocket_chat.websockethandlers.MainWebsocketHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class TestController {
 
+	private SimpMessagingTemplate template;
+
+	@Autowired
+	public TestController(SimpMessagingTemplate template) {
+		this.template = template;
+	}
+
 	private final Logger log = LoggerFactory.getLogger(TestController.class);
 
 	@MessageMapping("/ws-test")
-	public String handle(String t) {
+	public void handle(String t) {
 		log.info("message received: " + t);
 		//		return "[" + getTimestamp() + ": " + greeting;
-		return "[" + t + "] received";
+		String r = "[" + t + "] received";
+		this.template.convertAndSend("/queue", r);
 	}
 }
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,42 +1,55 @@
+// TODO: https://stomp-js.github.io/guide/stompjs/using-stompjs-v5.html
+
 const client = new StompJs.Client({
-    brokerURL: 'ws://localhost:8080/ws',
-    connectHeaders: {
-        // login: 'user',
-        // passcode: 'password',
-    },
-    debug: function (str) {
-        console.log(str);
-    },
-    reconnectDelay: 5000,
-    heartbeatIncoming: 4000,
-    heartbeatOutgoing: 4000,
+	brokerURL: 'ws://localhost:8080/ws',
+	connectHeaders: {
+		// login: 'user',
+		// passcode: 'password',
+	},
+	debug: function (str) { // DEBUG console output
+		console.log(str);
+	},
+	reconnectDelay: 5000,
+	heartbeatIncoming: 4000,
+	heartbeatOutgoing: 4000,
 });
 
-client.onConnect = function (frame) {
-    console.log("connected:\n", frame);
-    // Do something, all subscribes must be done is this callback
-    // This is needed because this will be executed after a (re)connect
+client.onConnect = async function (frame) {
+	console.log("connected:\n", frame);
+	// Do something, all subscribes must be done is this callback
+	// This is needed because this will be executed after a (re)connect
+	function cb(message) {
+		// called when the client receives a STOMP message from the server
+		if (message.body) {
+			console.log('got message with body ' + message.body);
+		} else {
+			console.log('got empty message');
+		}
+	}
 
-    sendMessage();
+	const headers = {ack: 'client'};
+	await client.subscribe('/queue', cb, headers);
+
+	sendMessage();
 };
 
 client.onStompError = function (frame) {
-    // Will be invoked in case of error encountered at Broker
-    // Bad login/passcode typically will cause an error
-    // Complaint brokers will set `message` header with a brief message. Body may contain details.
-    // Compliant brokers will terminate the connection after any error
-    console.log('Broker reported error: ' + frame.headers['message']);
-    console.log('Additional details: ' + frame.body);
+	// Will be invoked in case of error encountered at Broker
+	// Bad login/passcode typically will cause an error
+	// Complaint brokers will set `message` header with a brief message. Body may contain details.
+	// Compliant brokers will terminate the connection after any error
+	console.log('Broker reported error: ' + frame.headers['message']);
+	console.log('Additional details: ' + frame.body);
 };
 
 client.activate();
 
 function sendMessage() {
-    setInterval(() => {
-        client.publish({
-            destination: '/app/ws-test',
-            body: 'A A A A A',
-            skipContentLengthHeader: true,
-        });
-    }, 1000);
+	setInterval(() => {
+		client.publish({
+			destination: '/app/ws-test',
+			body: 'A A A A A',
+			skipContentLengthHeader: true,
+		});
+	}, 1000);
 }
