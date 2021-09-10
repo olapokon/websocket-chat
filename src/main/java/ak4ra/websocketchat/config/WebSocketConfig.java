8fc9ae454744c700d805ae795436ef8ca9fe3195
ak4ra <aryjna1@gmail.com>
2021-09-10 14:41:03 +0300

    Add DebugInterceptor

--- src/main/java/ak4ra/websocketchat/config/WebSocketConfig.java
+++ src/main/java/ak4ra/websocketchat/config/WebSocketConfig.java
@@ -4,9 +4,16 @@ import java.net.SocketAddress;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.simp.stomp.StompReactorNettyCodec;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.tcp.reactor.ReactorNettyTcpClient;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -48,6 +55,21 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     // TODO: Intercept messages if needed
     // https://docs.spring.io/spring-framework/docs/current/reference/html/web.html#websocket-stomp-interceptors
+    static class DebugInterceptor implements ChannelInterceptor {
+
+        @Override
+        public Message<?> preSend(Message<?> message, MessageChannel channel) {
+            StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+            return message;
+        }
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new DebugInterceptor());
+    }
+    // END intercept
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
