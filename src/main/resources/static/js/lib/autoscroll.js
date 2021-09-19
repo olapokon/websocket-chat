8ce722231e92f9039612454e8b7bbbc932146b09
ak4ra <aryjna1@gmail.com>
2021-09-19 19:26:52 +0300

    Refactor autoscroll

--- src/main/resources/static/js/lib/autoscroll.js
+++ src/main/resources/static/js/lib/autoscroll.js
@@ -1,11 +1,11 @@
 /**
  * @param e {HTMLElement} the HTML element that was mutated
- * @param m {MutationRecord} the mutation record
+ * @param addedNodes {NodeList} the nodes that have been added to the element
  */
-function autoScroll(e, m) {
-    if (m.addedNodes.length < 1)
-        return;
-    const addedElementHeight = m.addedNodes[0].parentElement.lastElementChild.clientHeight;
+function autoScroll(e, addedNodes) {
+    console.log(addedNodes);
+    const addedElementHeight = Array.from(addedNodes)
+        .reduce((height, node) => height + node.parentElement.lastElementChild.clientHeight, 0);
     const overflowHeight = e.scrollHeight - e.clientHeight;
     const isAtBottom = e.scrollHeight <= e.clientHeight
         || e.scrollTop >= e.scrollHeight - addedElementHeight - e.clientHeight;
@@ -15,16 +15,20 @@ function autoScroll(e, m) {
 }
 
 /**
- * Automatically scrolls down to the bottom of an HTML element, when the element is mutated,
+ * Automatically scrolls down to the bottom of an HTML element, when {@link Node}s are added to the element,
  * unless it is not already scrolled all the way down.
  *
  * @param e {HTMLElement} the HTML element to autoscroll
  */
-export default function onAppend(e) {
+export default function onAddNode(e) {
     const observer = new MutationObserver((mutations) =>
-        mutations.forEach(m => autoScroll(e, m)))
+        mutations.forEach(m => {
+            if (!m.addedNodes || m.addedNodes.length < 1)
+                return;
+            autoScroll(e, m.addedNodes);
+        }));
     observer.observe(e, {
         subtree: true,
         childList: true,
-    })
+    });
 }
