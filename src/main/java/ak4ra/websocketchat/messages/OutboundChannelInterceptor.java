b5e5cf723d2922bb2d27f65be9af968b4aad87e4
ak4ra <aryjna1@gmail.com>
2021-09-21 09:35:58 +0300

    Add mongodb docker

--- src/main/java/ak4ra/websocketchat/messages/OutboundChannelInterceptor.java
+++ src/main/java/ak4ra/websocketchat/messages/OutboundChannelInterceptor.java
@@ -2,7 +2,6 @@ package ak4ra.websocketchat.messages;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.SimpMessageType;
